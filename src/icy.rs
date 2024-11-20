d7b939d12bb328f9a32df33c0681656ab53f0de3
Dmitry Romanov <dmitry.romanov85@gmail.com>
2024-11-20 08:45:05 +0700

    move icy populate properties

--- src/icy.rs
+++ src/icy.rs
@@ -32,3 +32,32 @@ impl Properties {
         }
     }
 }
+
+pub fn populate_properties(properties: &mut Properties, headers: &[httparse::Header<'_>]) {
+    for header in headers {
+        let name = header.name.to_lowercase();
+        let name = name.as_str();
+        let val = std::str::from_utf8(header.value).unwrap_or("");
+
+        // There's a nice list here: https://github.com/ben221199/MediaCast
+        // Although, these were taken directly from Icecast's source: https://github.com/xiph/Icecast-Server/blob/master/src/source.c
+        match name {
+            "user-agent" => properties.uagent = Some(val.to_string()),
+            "ice-public" | "icy-pub" | "x-audiocast-public" | "icy-public" => {
+                properties.public = val.parse::<usize>().unwrap_or(0) == 1
+            }
+            "ice-name" | "icy-name" | "x-audiocast-name" => properties.name = Some(val.to_string()),
+            "ice-description" | "icy-description" | "x-audiocast-description" => {
+                properties.description = Some(val.to_string())
+            }
+            "ice-url" | "icy-url" | "x-audiocast-url" => properties.url = Some(val.to_string()),
+            "ice-genre" | "icy-genre" | "x-audiocast-genre" => {
+                properties.genre = Some(val.to_string())
+            }
+            "ice-bitrate" | "icy-br" | "x-audiocast-bitrate" => {
+                properties.bitrate = Some(val.to_string())
+            }
+            _ => (),
+        }
+    }
+}
